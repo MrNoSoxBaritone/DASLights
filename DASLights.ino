8574f833c5bffbee4e0bf9f501d0d632aecfa61c
Simon Axon <Simon.Axon@warwick.ac.uk>
2017-02-06 01:07:36 +0000

    Revert "Initial commit on LCD addition"
    This reverts commit 8c0eaea9790f88530e2aeb5054ad51648ea2f5e2.

--- DASLights.ino
+++ DASLights.ino
@@ -24,7 +24,7 @@
   Is the Emergency Stop causing three beeps??
   
  */
-#define VERSION "1.9.3" // Starting work on LCD integration, testing Git branching too...
+#define VERSION "1.9.2"
 
 // Compilation flag, uncomment for hardware-free test mode
 #define EMULATIONMODE 0
